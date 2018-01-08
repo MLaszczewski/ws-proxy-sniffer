857244c7ee42e43c8f7bfc1cfd670ccbbee79e25
Michał Łaszczewski <michal@emikse.com>
2018-01-08 19:06:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+HTTP Proxy for websocket debugging
+======
+
+usage: node index.js <localPort> <remoteHost> <remotePort>
---
+++ index.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+if(process.argv.length<5) {
+  console.log(`USAGE: node ${argv[1]} <localPort> <remoteHost> <remotePort>`)
+}
+
+const localPort = process.argv[2]
+const remoteHost = process.argv[3]
+const remotePort = process.argv[4]
+
+const http = require('http')
+const moment = require('moment')
+const websocket = require('websocket')
+
+let lastLog = Date.now()
+
+function log(addr, ...args) {
+  let now = Date.now()
+  let delay = now - lastLog
+  lastLog = now
+  console.log('['+moment().format("DD.MM.YYYY HH:mm:ss")+']', '[ after '+delay+'ms ]', `[${addr}]`, ...args)
+}
+
+var server = http.createServer(function (creq, cres) {
+  log(creq.connection.remoteAddress, 'request', creq.url, creq.method)
+  creq.headers.host = `${remoteHost}:${remotePort}`
+  //log(creq.connection.remoteAddress, "headers", creq.headers)
+
+  var options = {
+    hostname: remoteHost,
+    port: remotePort,
+    path: creq.url,
+    method: creq.method,
+    headers: creq.headers
+  }
+
+  var proxy = http.request(options, function (res) {
+    log(creq.connection.remoteAddress, 'response', creq.url, res.statusCode, res.statusMessage)
+    res.pipe(cres, {
+      end: true
+    })
+  })
+
+  creq.pipe(proxy, {
+    end: true
+  })
+})
+
+
+wsServer = new websocket.server({
+  httpServer: server,
+  autoAcceptConnections: false
+})
+
+
+wsServer.on('request', function(request) {
+
+  let remoteWS = `ws://${remoteHost}:${remotePort}${request.resourceURL.href}`
+  log(request.remoteAddress, 'websocket request', request.resourceURL.href, '-->', remoteWS)
+
+  let client = new websocket.client()
+
+  client.on('connectFailed', function(error) {
+    log(request.remoteAddress, 'websocket server connection failed: ', error.toString())
+  })
+
+  client.on('connect', function(connection) {
+    var clientConnection = request.accept(connection.protocol, request.origin)
+    log(request.remoteAddress, 'websocket connected, protocol:', JSON.stringify(connection.protocol))
+
+    connection.on('error', function(error) {
+      log(request.remoteAddress, 'websocket server connection error: ', error.toString())
+      clientConnection.close()
+    })
+    connection.on('close', function() {
+      log(request.remoteAddress, 'websocket server connection closed by server')
+      clientConnection.close()
+    })
+    connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+        clientConnection.sendUTF(message.utf8Data)
+        log(request.remoteAddress, "frame from server:\n", message.utf8Data);
+      }
+      if (message.type === 'binary') {
+        clientConnection.sendBytes(message.binaryData)
+        log(request.remoteAddress, "binary frame from server");
+      }
+    })
+
+    clientConnection.on('message', function(message) {
+      if (message.type === 'utf8') {
+        connection.sendUTF(message.utf8Data)
+        log(request.remoteAddress, "frame from client:\n", message.utf8Data);
+      }
+      if (message.type === 'binary') {
+        connection.sendBytes(message.binaryData)
+        log(request.remoteAddress, "binary frame from client");
+      }
+    })
+    clientConnection.on('close', function(reasonCode, description) {
+      log(request.remoteAddress, 'websocket connection closed by client')
+      connection.close()
+    })
+  })
+
+  client.connect(remoteWS, request.requestedProtocols)
+})
+
+server.listen(localPort);
+
+log("Proxy listening on port "+localPort)
